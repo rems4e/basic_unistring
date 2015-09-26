fced1167c572f7d141ab8218813d8bf82197d2e7
Rémi <saurel@etud.insa-toulouse.fr>
2015-09-26 20:12:29 +0200

    Initial commit

---
+++ unistring.cpp
@@ -0,0 +1,135 @@
+//
+//  unistring.cpp
+//  RPG 3D
+//
+//  Created by Rémi on 22/09/2015.
+//
+//
+
+#include "unistring.h"
+#include <codecvt>
+#include "utf8proc.h"
+#include <algorithm>
+#include <sstream>
+#include <array>
+
+namespace {
+    template <typename FromType, typename ToType>
+    std::basic_string<ToType>
+    convertIn(basic_unistring<FromType> const &internal, std::codecvt<FromType, ToType, std::mbstate_t> const &codecvt);
+
+    template <typename FromType, typename ToType>
+    std::basic_string<FromType>
+    convertOut(basic_unistring<ToType> const &internal, std::codecvt<FromType, ToType, std::mbstate_t> const &codecvt);
+}
+
+template <>
+template <typename CharType2, typename>
+basic_unistring<char>::basic_unistring(basic_unistring<CharType2> const &str)
+        : basic_string<char>(str.to_utf8()) {}
+
+template <>
+template <typename CharType2, typename>
+basic_unistring<char16_t>::basic_unistring(basic_unistring<CharType2> const &str)
+        : basic_string<char16_t>(str.to_utf16()) {}
+
+template <>
+template <typename CharType2, typename>
+basic_unistring<char32_t>::basic_unistring(basic_unistring<CharType2> const &str)
+        : basic_string<char32_t>(str.to_utf32()) {}
+
+template <typename CharType, typename T>
+basic_unistring<CharType, T> basic_unistring<CharType, T>::get_normalized() const {
+    return reinterpret_cast<char const *>(utf8proc_NFC(reinterpret_cast<utf8proc_uint8_t const *>(to_utf8().c_str())));
+}
+
+template <>
+template <>
+std::string basic_unistring<char16_t>::to_utf8<false>() const {
+    return convertIn(*this, std::codecvt_utf8_utf16<char_type>{});
+}
+
+template <>
+template <>
+std::string basic_unistring<char32_t>::to_utf8<false>() const {
+    return convertIn(*this, std::codecvt_utf8<char_type>{});
+}
+
+template <>
+template <>
+std::u16string basic_unistring<char>::to_utf16<false>() const {
+    return convertOut(*this, std::codecvt_utf8_utf16<char16_t>{});
+}
+
+template <>
+template <>
+std::basic_string<char16_t> basic_unistring<char32_t>::to_utf16<false>() const {
+    return basic_unistring<char>(this->to_utf8()).to_utf16();
+}
+
+template <>
+template <>
+std::u32string basic_unistring<char>::to_utf32<false>() const {
+    return convertOut(*this, std::codecvt_utf8<char32_t>{});
+}
+
+template <>
+template <>
+std::basic_string<char32_t> basic_unistring<char16_t>::to_utf32<false>() const {
+    return basic_unistring<char>(this->to_utf8()).to_utf32();
+}
+
+template <typename CharType, typename T>
+bool basic_unistring<CharType, T>::starts_with(basic_unistring<CharType> const &substr) {
+    return this->size() >= substr.size() && std::equal(substr.begin(), substr.end(), this->begin());
+}
+
+template <typename CharType, typename T>
+bool basic_unistring<CharType, T>::ends_with(basic_unistring<CharType> const &substr) {
+    return this->size() >= substr.size() && std::equal(substr.rbegin(), substr.rend(), this->rbegin());
+}
+
+namespace {
+    template <typename FromType, typename ToType>
+    std::basic_string<ToType>
+    convertIn(basic_unistring<FromType> const &internal, std::codecvt<FromType, ToType, std::mbstate_t> const &codecvt) {
+        std::string external(internal.size() * codecvt.max_length(), '\0');
+        const FromType *from_next;
+        std::mbstate_t mb = std::mbstate_t();
+        ToType *to_next;
+        codecvt.out(mb, internal.data(), internal.data() + internal.size(), from_next, &external[0], &external[external.size()], to_next);
+        external.resize(to_next - &external[0]);
+
+        return external;
+    }
+
+    template <typename FromType, typename ToType>
+    std::basic_string<FromType>
+    convertOut(basic_unistring<ToType> const &internal, std::codecvt<FromType, ToType, std::mbstate_t> const &codecvt) {
+        std::basic_string<FromType> external(internal.size() * codecvt.max_length(), '\0');
+        const ToType *from_next;
+        std::mbstate_t mb = std::mbstate_t();
+        FromType *to_next;
+        codecvt.in(mb, internal.data(), internal.data() + internal.size(), from_next, &external[0], &external[external.size()], to_next);
+        external.resize(to_next - &external[0]);
+
+        return external;
+    }
+}
+
+basic_unistring<char> operator""_u8(char const *str, size_t length) { return { str }; }
+
+basic_unistring<char16_t> operator""_u16(char const *str, size_t length) { return { str }; }
+
+basic_unistring<char32_t> operator""_u32(char const *str, size_t length) { return { str }; }
+
+template class basic_unistring<char>;
+template class basic_unistring<char16_t>;
+template class basic_unistring<char32_t>;
+
+template basic_unistring<char>::basic_unistring(basic_unistring<char16_t> const &);
+template basic_unistring<char>::basic_unistring(basic_unistring<char32_t> const &);
+template basic_unistring<char16_t>::basic_unistring(basic_unistring<char> const &);
+template basic_unistring<char16_t>::basic_unistring(basic_unistring<char32_t> const &);
+template basic_unistring<char32_t>::basic_unistring(basic_unistring<char> const &);
+template basic_unistring<char32_t>::basic_unistring(basic_unistring<char16_t> const &);
---
+++ unistring.h
@@ -0,0 +1,304 @@
+//
+//  unistring.hpp
+//  RPG 3D
+//
+//  Created by Rémi on 22/09/2015.
+//
+//
+
+#ifndef unistring_hpp
+#define unistring_hpp
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <experimental/type_traits>
+#include <sstream>
+#include <array>
+namespace std {
+    using namespace std::experimental;
+}
+
+template <typename CharType, typename _T = std::enable_if_t<std::is_same_v<CharType, char> || std::is_same_v<CharType, char16_t> || std::is_same_v<CharType, char32_t>>>
+class basic_unistring : private std::basic_string<CharType> {
+    using base_type = std::basic_string<CharType>;
+
+public:
+    using typename base_type::value_type;
+    using char_type = typename base_type::value_type;
+
+    using typename base_type::size_type;
+    using typename base_type::difference_type;
+
+    using typename base_type::reference;
+    using typename base_type::const_reference;
+    using typename base_type::pointer;
+    using typename base_type::const_pointer;
+
+    using typename base_type::iterator;
+    using typename base_type::const_iterator;
+    using typename base_type::reverse_iterator;
+    using typename base_type::const_reverse_iterator;
+
+    using typename base_type::allocator_type;
+
+
+    using base_type::npos;
+
+    using std::basic_string<CharType>::basic_string;
+
+    template <typename CharType2, typename... Args>
+    static basic_unistring createWithFormat(basic_unistring<CharType2> format, Args &&... args) {
+        basic_unistring string;
+        string.appendFormat(std::forward<Args &&>(args)...);
+
+        return string;
+    }
+
+    /**
+     * Constructs the basic_unistring object from the given null-terminated, wide or narrow char array, performaing the necessary
+     * conversion.
+     */
+    template <typename CharType2, typename = std::enable_if_t<!std::is_same_v<CharType, CharType2>>>
+    basic_unistring(CharType2 const *str)
+            : basic_unistring(std::basic_string<CharType2>(str)) {}
+
+    /**
+     * Constructs the basic_unistring object from the given basic_unistring, performing the necessary conversion.
+     */
+    template <typename CharType2, typename = std::enable_if_t<!std::is_same_v<CharType, CharType2>>>
+    basic_unistring(basic_unistring<CharType2> const &str);
+
+    /**
+     * Constructs the basic_unistring object from the given basic_string, performing the necessary conversion.
+     */
+    basic_unistring(std::basic_string<CharType> const &str) : std::basic_string<CharType>(str) {}
+    template <typename CharType2, typename = std::enable_if_t<!std::is_same_v<CharType, CharType2>>>
+    basic_unistring(std::basic_string<CharType2> const &str)
+            : basic_unistring<CharType>(basic_unistring<CharType2>(str)) {}
+
+    template <typename T>
+    basic_unistring &operator=(T const &value) {
+        using std::swap;
+        basic_unistring tmp(value);
+        swap(*this, tmp);
+
+        return *this;
+    }
+    template <typename T>
+    basic_unistring &assign(T const &value) {
+        return *this = value;
+    }
+
+    using base_type::begin;
+    using base_type::end;
+    using base_type::cbegin;
+    using base_type::cend;
+    using base_type::rbegin;
+    using base_type::rend;
+    using base_type::crbegin;
+    using base_type::crend;
+
+    using base_type::front;
+    using base_type::back;
+
+    template <typename T>
+    basic_unistring &append(T const &value) {
+        return *this += value;
+    }
+
+    template <typename T>
+    basic_unistring &operator+=(T const &value) {
+        basic_unistring tmp(value);
+        this->basic_string::operator+=(tmp);
+        return *this;
+    }
+
+    using base_type::at;
+    using base_type::operator[];
+
+    using base_type::c_str;
+    using base_type::data;
+
+    using base_type::size;
+    using base_type::length;
+    using base_type::empty;
+
+    using base_type::capacity;
+    using base_type::reserve;
+    using base_type::max_size;
+    using base_type::shrink_to_fit;
+
+    using base_type::clear;
+    using base_type::insert;
+    using base_type::erase;
+    using base_type::push_back;
+    using base_type::pop_back;
+
+    using base_type::compare;
+    using base_type::replace;
+    basic_unistring substr(size_type pos = 0, size_type n = npos) const {
+        return basic_unistring(*this, pos, n);
+    }
+    using base_type::copy;
+    using base_type::resize;
+    using base_type::swap;
+
+    using base_type::find;
+    using base_type::rfind;
+    using base_type::find_first_of;
+    using base_type::find_first_not_of;
+    using base_type::find_last_of;
+    using base_type::find_last_not_of;
+
+    template <bool = std::is_same_v<CharType, char>>
+    std::string to_utf8() const {
+        return { begin(), end() };
+    }
+
+    template <bool = std::is_same_v<CharType, char16_t>>
+    std::u16string to_utf16() const {
+        return { begin(), end() };
+    }
+
+    template <bool = std::is_same_v<CharType, char32_t>>
+    std::u32string to_utf32() const {
+        return { begin(), end() };
+    }
+
+    operator std::string() const {
+        return to_utf8();
+    }
+
+    operator std::basic_string<char16_t>() const {
+        return to_utf16();
+    }
+
+    operator std::basic_string<char32_t>() const {
+        return to_utf32();
+    }
+
+    void normalize() {
+        *this = this->get_normalized();
+    }
+
+    basic_unistring get_normalized() const;
+
+    template <template <typename> class Container = std::vector>
+    Container<basic_unistring> split(CharType separator) const {
+        return this->split(basic_unistring{ separator });
+    }
+
+    template <template <typename T> class Container = std::vector>
+    Container<basic_unistring> split(basic_unistring const &separator) const;
+
+    template <typename CharType2>
+    bool starts_with(basic_unistring<CharType2> const &substr) {
+        return this->startsWith(basic_unistring{ substr });
+    }
+    bool starts_with(basic_unistring<CharType> const &substr);
+
+    template <typename CharType2>
+    bool ends_with(basic_unistring<CharType2> const &substr) {
+        return this->ends_with(basic_unistring{ substr });
+    }
+    bool ends_with(basic_unistring<CharType> const &substr);
+
+    template <typename CharType2, typename... Args>
+    basic_unistring &append_format(basic_unistring<CharType2> format, Args &&... args) {
+        return this->append_format(basic_unistring{ format }, args...);
+    }
+
+    template <typename CharType2, typename... Args>
+    basic_unistring &append_format(std::basic_string<CharType2> format, Args &&... args) {
+        return this->append_format(basic_unistring{ format }, args...);
+    }
+
+    template <typename... Args>
+    basic_unistring &append_format(basic_unistring format, Args &&... args);
+
+private:
+    static void map_arguments(std::vector<basic_unistring> &vect) {}
+
+    template <typename Arg, typename... Args>
+    static void map_arguments(std::vector<basic_unistring> &vect, Arg &&arg, Args &&... args);
+};
+
+template <typename CharType, typename T>
+template <template <typename> class Container>
+Container<basic_unistring<CharType, T>> basic_unistring<CharType, T>::split(basic_unistring<CharType, T> const &separator) const {
+    Container<basic_unistring<CharType, T>> container;
+    size_type start = 0;
+    auto pos = this->find(separator, start);
+    while(pos != basic_unistring::npos) {
+        container.emplace_back(*this, start, pos - start);
+        start = pos + 1;
+        pos = this->find(separator, start);
+    }
+    if(start < this->length())
+        container.emplace_back(*this, start, this->size() - start);
+
+    return container;
+}
+
+template <typename CharType, typename T>
+template <typename Arg, typename... Args>
+void basic_unistring<CharType, T>::map_arguments(std::vector<basic_unistring> &vect, Arg &&arg, Args &&... args) {
+    std::stringstream ss;
+    ss << arg;
+    vect.push_back(ss.str());
+
+    mapArguments(vect, args...);
+}
+
+template <typename CharType, typename T>
+template <typename... Args>
+basic_unistring<CharType, T> &basic_unistring<CharType, T>::append_format(basic_unistring format, Args &&... args) {
+    std::vector<basic_unistring> formattedArgs;
+    basic_unistring::map_arguments(formattedArgs, args...);
+
+    size_type start = 0;
+    auto pos = format.find((CharType)'{', start);
+    while(pos != basic_unistring::npos) {
+        start = pos + 1;
+        pos = format.find((CharType)'{', start);
+        if(pos != start) {
+            auto end = format.find((CharType)'}', start + 1);
+            if(end != npos) {
+                size_t index;
+                std::stringstream str(format.substr(start, end - start).toUtf8());
+                str >> index;
+                if(str.fail() || !str.eof()) {
+                    index = -1;
+                }
+                auto const &arg = formattedArgs.at(index);
+                auto sizeDiff = arg.size() - (end - start);
+                format.replace(start - 1, end - start + 2, arg);
+                start += sizeDiff;
+            }
+        } else {
+            ++start;
+        }
+        pos = format.find((CharType)'{', start);
+    }
+
+    this->append(format);
+
+    return *this;
+}
+
+basic_unistring<char> operator""_u8(char const *str, size_t length);
+basic_unistring<char16_t> operator""_u16(char const *str, size_t length);
+basic_unistring<char32_t> operator""_u32(char const *str, size_t length);
+
+extern template class basic_unistring<char>;
+extern template class basic_unistring<char16_t>;
+extern template class basic_unistring<char32_t>;
+
+template <typename CharType>
+std::ostream &operator<<(std::ostream &stream, basic_unistring<CharType> const &str) {
+    stream << str.to_utf8();
+    return stream;
+}
+
+#endif /* unistring_hpp */
